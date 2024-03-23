97bffb5330ff5ac53a029c254cf3b99040b865a8
Ali-Hassan6626 <142166998+Ali-Hassan6626@users.noreply.github.com>
2024-03-23 19:58:15 +0500

    envirement created

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-import './bootstrap';
+require('./bootstrap');
+
+window.Vue = require('vue').default;
+//global methods file
+import './globalMethods';
+
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+Vue.use(DatePicker)
+Vue.component('date-picker', DatePicker)
+
+
+import './adminComponentsRegister';
+
+import './employeeComponentsRegister';
+
+const app = new Vue({
+    el: '#app',
+});
