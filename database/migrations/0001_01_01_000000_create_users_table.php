693a80e4459e258b3aced2f2fd0352e6e14c5caa
khubaib-khan <khubaib.islam@innovotechnologies.com>
2024-03-25 20:15:29 +0500

    migration changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('twitter_id')->unique();
+            $table->longText('profile_img')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
