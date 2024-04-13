337a59fced78187060846119f60ee70372e5099e
khubaib-khan <khubaib.islam@innovotechnologies.com>
2024-04-13 13:03:19 +0500

    vue components

---
+++ resources/js/adminComponent.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+
+Vue.component('points-index', require('./components/points/Index.vue').default);
+Vue.component('points-model', require('./components/points/PointModel.vue').default);
