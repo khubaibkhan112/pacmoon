d22f3da85d2af5ad30ff5dbcaa684cc749695e20
khubaib-khan <khubaib.islam@innovotechnologies.com>
2024-04-17 17:23:24 +0500

    vue settings

--- resources/js/adminComponent.js
+++ resources/js/adminComponent.js
@@ -1,9 +1,11 @@
 
 import { createApp } from 'vue';
 import pointsIndex from './components/points/Index.vue';
+// import pointModel from './components/points/PointModel.vue';
 
 const app = createApp({});
 app.component('pointsIndex', pointsIndex);
+// app.component('pointModel', pointModel);
 
 
 export default app;
