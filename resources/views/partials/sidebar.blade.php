4dd5c78e091fb680de94224127e69d9a6958a3b1
Ali-Hassan6626 <142166998+Ali-Hassan6626@users.noreply.github.com>
2024-05-14 18:37:20 +0500

    icons changed in sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,7 +2,7 @@
   <div class="app-brand demo">
     <a href="{{url('/')}}" class="app-brand-link">
       <span class="app-brand-logo demo">
-        <img src="https://pbs.twimg.com/profile_images/1469866962645880834/-uD_orAt_normal.jpg" class="rounded-full" alt="mingo" height="22" width="32" >
+        <img src="https://pbs.twimg.com/profile_images/1469866962645880834/-uD_orAt_normal.jpg" class="rounded-full" alt="mingo" height="22" width="32">
       </span>
       <span class="app-brand-text demo menu-text fw-bold">Pacmoon</span>
     </a>
@@ -19,19 +19,19 @@
     <!-- Dashboards -->
     <li class="menu-item">
       <a href="{{route('home')}}" class="menu-link">
-        <i class="menu-icon tf-icons ti ti-smart-home"></i>
+        <i class="menu-icon tf-icons ti ti-home"></i>
         <div data-i18n="Dashboards">Dashboards</div>
       </a>
     </li>
     <li class="menu-item">
       <a href="{{route('points.index')}}" class="menu-link {{request()->segment(1)=="points" ? 'active' : ''}}">
-        <i class="menu-icon tf-icons ti ti-smart-home"></i>
+        <i class="menu-icon tf-icons ti ti-star"></i>
         <div data-i18n="Points">Points</div>
       </a>
     </li>
     <li class="menu-item">
       <a href="{{route('quests.index')}}" class="menu-link {{request()->segment(1)=="quests" ? 'active' : ''}}">
-        <i class="menu-icon tf-icons ti ti-smart-home"></i>
+        <i class="menu-icon tf-icons ti ti-shield-question"></i>
         <div data-i18n="Quests">Quests</div>
       </a>
     </li>
